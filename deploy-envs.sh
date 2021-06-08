9b0fc4b5cfa2684747cdad48e76104ed88b34193
cheesiewalters <cwalters01@qub.ac.uk>
2021-06-08 02:08:50 +0100

    inital changes

---
+++ deploy-envs.sh
@@ -0,0 +1,8 @@
+
+
+export IMAGE_NAME=ecsdemo-redisgeo
+export IMAGE_VERSION=latest
+
+export AWS_DEFAULT_REGION=us-east-1
+export AWS_ECS_CLUSTER_NAME=default
+export AWS_VIRTUAL_HOST=34.236.162.140
